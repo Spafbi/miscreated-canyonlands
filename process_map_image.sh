70a0201bc1fd3eaadb0c709231a815c0e476a001
Spafbi <chris.snow@spafbi.com>
2020-02-03 09:49:48 -0600

    Updated process_map_image script for canyonlands

--- process_map_image.sh
+++ process_map_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if [ ! -f ./islands_height.png ]; then
-  echo "islands_height.png not found. No action taken"
+if [ ! -f ./canyonlands.png ]; then
+  echo "canyonlands.png not found. No action taken"
   exit
 fi
-gdal2tiles.py -p raster -z 2-6 -w none islands_height.png
-find ./islands_height/ -type f -name '*.png' -exec pngquant {} --ext .png -f -s1 \;
+gdal2tiles.py -p raster -z 2-6 -w none canyonlands.png
+find ./canyonlands/ -type f -name '*.png' -exec pngquant {} --ext .png -f -s1 \;
